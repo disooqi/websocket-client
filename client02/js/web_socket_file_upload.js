18b3ad0ca6daf08843adc05b56bd317a05e6402f
Mohamed Eldesouki <mohamohamed@hbku.edu.qa>
2018-02-20 15:44:55 +0300

    Add client02: a client that uses websocket to send a binary files to the server

---
+++ client02/js/web_socket_file_upload.js
@@ -0,0 +1,57 @@
+(function(window, document) {
+
+    var socket, statusLog;
+
+    document.addEventListener("DOMContentLoaded", function() {
+
+        statusLog = document.getElementById("status-log");
+        statusLog.innerText += "\n";
+
+        socket = new WebSocket("ws://localhost:8888/ws");
+        socket.addEventListener("open", function() {
+            console.log("connection open");
+        });
+
+        socket.addEventListener("message", function(msg) {
+            statusLog.innerText += msg.data + "\n";
+        });
+
+    });
+
+    document.addEventListener("dragenter", function(e) {
+        e.preventDefault();
+        e.target.classList.add("active");
+    });
+
+    document.addEventListener("dragleave", function(e) {
+        e.preventDefault();
+        e.target.classList.remove("active");
+    })
+
+    document.addEventListener("dragover", function(e) {
+        e.preventDefault();
+    });
+
+    document.addEventListener("drop", function(e) {
+
+        e.preventDefault();
+        e.target.classList.remove("active");
+
+        for (var x=0; x<e.dataTransfer.files.length; x++) {
+
+            var
+                file = e.dataTransfer.files[x],
+                //file = "/home/disooqi/PycharmProjects/arabic_test.wav",
+                fr = new FileReader();
+
+            fr.addEventListener("loadend", function() {
+                statusLog.innerText += "sending " + fr.result.byteLength + " bytes" + "\n";
+                socket.send(fr.result);
+            });
+
+            fr.readAsArrayBuffer(file);
+        }
+
+    });
+
+})(window, document);
